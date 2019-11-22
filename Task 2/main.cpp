6ca842bb2a0b8bc01757c7b96cbf7e68bbee2213
Karashily <ahmed.elkarashily@gmail.com>
2019-11-23 01:30:39 +0200

    code review

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -3,54 +3,62 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include<fstream>
+#include <fstream>
+
 using namespace std;
+
 void handler(int signum);
-int main()
-{
+
+int main() {
 	signal(SIGCHLD , handler);
+	
 	vector<int> numbers;
 	vector<int> pids;
-	ifstream fin; fin.open("file.txt");
+	
+	ifstream fin;
+	fin.open("file.txt");
+	
 	int num;
 	while(fin >> num) numbers.push_back(num);
+	
 	int numChilds = 3;
 	int block = (int) numbers.size() / numChilds;
 	int pid = 1;
 	int i = 0;
 	while(pid && i++ < numChilds) {
-		pid = fork() ; 
+		pid = fork();
 		if(pid) pids.push_back(pid);	
 	}
+	
 	int sum = 0;
-	if(!pid){
+	if (!pid) {
 		int cnt = 0;
-		for(int j = (i-1)*block ; cnt++ < block && j < (int) numbers.size();j++ ){
-			sum+=numbers[j];
+		for (int j = (i - 1) * block;
+		 cnt++ < block && j < (int) numbers.size();
+		 j++) {
+			sum += numbers[j];
 		}
-		printf("Child number %d with pid = %d -> %d\n" , i , getpid() , sum);
+		printf("Child number %d with pid = %d -> %d\n", i, getpid(), sum);
 		pause();
 	}
-	else{
-		sleep(10);
-		for(int j = 0 ; j < numChilds ; ++j){
-			kill(pids[j] , 9);
-			for(int k = 0 ; k < 2 ; ++k ) sleep(1);
+	else {
+		int sleepDuration = 10;
+		while(sleepDuration--) sleep(1);
+		for(int j = 0; j < numChilds; ++j) {
+			kill(pids[j], 9);
+			for(int k = 0; k < 2; ++k) sleep(1);
 		}
-		printf("Good Bye World !\n");
-		
+		printf("Good Bye World !\n");	
 	}
-	
 	return 0;
 }
-void handler(int signum){
-	while (1) 
-  	{  
+
+void handler(int signum) {
+	while (1) {  
       		int status;
-      		int pid = waitpid(-1 , &status, WNOHANG);
+      		int pid = waitpid(-1, &status, WNOHANG);
       		if (pid <= 0)  break;
-     		printf("Child has sent a SIGCHLD signal #%d\n",signum);    
+     		printf("Child has sent a SIGCHLD signal #%d\n", signum);    
      		printf("A child with pid %d terminated with exit code %d\n", pid, status>>8);
-    		
   	}	
 }
