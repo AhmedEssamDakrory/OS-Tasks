753644f033af6895c0a958e5563ed9d0baf7d541
karashily <ahmed.elkarashily@gmail.com>
2019-12-24 21:58:35 +0200

    shm without sem

--- Task3/shm.c
+++ Task3/shm.c
@@ -1,86 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
+
+#define PERIOD		1000000
 
 
-incrementor(int shmid)
-{
-  void *shmaddr;
-  
-  for(int i = 0; i < 1000000 ;i++) {
-    shmaddr = shmat(shmid, (void *)0, 0);
-    if(shmaddr == -1)
-    {	
-      perror("Error in attach in writer");
-      exit(-1);
+int Incrementor(int shmid) {
+  int *shmaddr, ReadValue;
+  int i;
+  char quit_message[5];
+   
+  shmaddr = (int*) shmat(shmid, (void *)0, 0);
+  if(shmaddr == -1) {	
+  	perror("Error in attach in Incrementor");
+  	exit(-1);
+  }
+  else {
+    for (i=0; i<PERIOD; i++){
+      ReadValue = *shmaddr;
+      *shmaddr = ReadValue + 1;
     }
-    else
-    {
-      int* count = (int*) shmaddr;
-      *count++;
-      
-      if (i == 999999)
-      {
-        printf("parent value = %d", *count);
-      }
-      
-      shmdt(shmaddr);
-    }  	
-  }  
+
+    printf("Incrementor: Our shared value is %d now\n", *shmaddr);	    
+
+    sleep(2);
+    printf("\nFinal Value is %d\n", *shmaddr);
+    
+    shmdt(shmaddr);
+  }  	 
 }
 
-decrementor(int shmid)
-{
-  void *shmaddr;
+int Decrementor(int shmid) {
+  int *shmaddr, ReadValue, i;
   
-  for(int i = 0; i < 1000000 ;i++) {
-    shmaddr = shmat(shmid, (void *)0, 0);
-    if(shmaddr == -1)
-    {	
-      perror("Error in attach in reader");
-      exit(-1);
+  shmaddr = (int*) shmat(shmid, (void *)0, 0);
+  if(shmaddr == -1) {	
+  	perror("Error in attach in Decrementor");
+  	exit(-1);
+  }
+  else {
+    for(i=0; i<PERIOD; i++){
+      ReadValue = *shmaddr;	    
+      *shmaddr = ReadValue - 1;
     }
-    else
-    {
-      int* count = (int*) shmaddr;
-      *count--;
-      
-      if (i == 999999)
-      {
-        printf("child value = %d", *count);
-      }
 
-      shmdt(shmaddr);
-    }
+    printf("Decrementor: Our shared value is %d now\n", *shmaddr);
+
+    shmdt(shmaddr);
   }
 }
 
-main()
-{
-  int shmid, pid;
+int main() {
+  int shmid, pid, *shmaddr;
   
-  shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT|0644);
+  // getting the shared memory
+  shmid = shmget(IPC_PRIVATE, 10, IPC_CREAT|0644);
   
-  if(shmid == -1)
-  	{
-  	  perror("Error in create");
-  	  exit(-1);
-  	}
-  else
-  	printf("\nShared memory ID = %d\n", shmid);
-     	
+  if(shmid == -1) {
+    perror("Error in create shm");
+    exit(-1);
+  }
+  else {
+   	printf("Shared memory ID = %d\n", shmid);
+  }
+  
+  shmaddr = (int*) shmat(shmid, (void *)0, 0);
+  if(shmaddr == -1) {	
+  	perror("Error in attach in parent");
+  	exit(-1);
+  }
+  else {	
+   	*shmaddr = 0;		/* initialize shared memory */
+   	shmdt(shmaddr);
+  }
+  
   pid = fork();
   
-  if(pid == 0)
-  	decrementor(shmid);
-  else if(pid != -1)
-  	incrementor(shmid);
-  else
-    {
-  	perror("Error in fork");
-  	exit(-1);
-    }
+  if(pid == 0) {
+  	Decrementor(shmid);
+  }
+  else if(pid != -1) {
+  	Incrementor(shmid);
+    wait(NULL);
+  }
+  else {
+    perror("Error in fork");
+    exit(-1);
+  }
 }
